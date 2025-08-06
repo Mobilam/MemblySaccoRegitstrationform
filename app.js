836630bd829c4eb23667013e4b6333dfa4b4e1b3
Mobi <mwaibruce@gmail.com>
2025-08-07 01:58:27 +0300

    New changes unofficial

---
+++ app.js
@@ -0,0 +1,5 @@
+document.getElementById("saccoForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  alert("Thank you! Your application has been recorded.");
+  // Here you can plug in logic to send data via Fetch API or form service
+});
